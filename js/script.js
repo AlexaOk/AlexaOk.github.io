e33605734ee0fa55fc0ef84358956016af1005ec
Alexa <alexa.okito@epitech.eu>
2018-05-03 17:39:22 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,53 @@
+jQuery("#cupcake").fadeOut("slow");
+
+jQuery("#hoverDiv").mouseenter(function(){
+    jQuery("#hiddenDiv").slideDown();
+}).mouseleave(function(){
+    jQuery("#hiddenDiv").slideUp();
+})
+
+// set up text to print, each item in array is new line
+var aText = new Array(
+"Alexa Okito",
+"Développeuse Web",
+"Full Stack"
+);
+var iSpeed = 100; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
+
+function typewriter()
+{
+ sContents =  ' ';
+ iRow = Math.max(0, iIndex-iScrollAt);
+ var destination = document.getElementById("typedtext");
+
+ while ( iRow < iIndex ) {
+  sContents += aText[iRow++] + '<br />';
+ }
+ destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+ if ( iTextPos++ == iArrLength ) {
+  iTextPos = 0;
+  iIndex++;
+  if ( iIndex != aText.length ) {
+   iArrLength = aText[iIndex].length;
+   setTimeout("typewriter()", 500);
+  }
+ } else {
+  setTimeout("typewriter()", iSpeed);
+ }
+}
+
+
+typewriter();
+
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+      
---
+++ loader.php
@@ -0,0 +1,19 @@
+<div id = "cupcake" class = "box">
+
+  <span class = "letter box">A</span>
+  <span class = "letter box">L</span>
+  <span class = "letter box">E</span>
+  <span class = "letter box">X</span>
+  <span class = "letter box">A</span>
+  &nbsp; &nbsp; &nbsp;
+  <div class = "cupcakeCircle box">
+    <div class = "cupcakeInner box">
+      <div class = "cupcakeCore box"></div>
+
+    </div>
+  </div>
+  <span class = "letter box">K</span>
+  <span class = "letter box">I</span>
+  <span class = "letter box">T</span>
+  <span class = "letter box">O</span>
+</div>
